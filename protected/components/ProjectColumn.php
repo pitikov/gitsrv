1f73bd78c12a31ca0916364f8ccf80f8eaf393f0
root <root@samsung-q70.ovp.po.nio1>
2014-01-29 16:56:09 +0400

    Добавил всплывающую подсказку

--- protected/components/ProjectColumn.php
+++ protected/components/ProjectColumn.php
@@ -4,7 +4,7 @@ class ProjectColumn extends CDataColumn {
 
   protected function renderDataCellContent($row, $data)
   {
-		echo CHtml::link($data['id'], Yii::app()->createUrl('/project/webgit', array('project'=>$data['id'])));
+		echo CHtml::link($data['id'], Yii::app()->createUrl('/project/webgit', array('project'=>$data['id'])), array('title'=>'Просмотр репозитория проекта '.$data['id']));
   }
   
 }
